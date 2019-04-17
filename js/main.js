7162df0b6b7d3a475013b194c97771ba2b83898c
kitzeller <kit.zeller@gmail.com>
2019-04-17 07:51:54 -0400

    Added website template

--- js/main.js
+++ js/main.js
@@ -1,57 +1,93 @@
-function main(){
-    var margin = {top: 10, right: 30, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+/*global $, jQuery, alert*/
+$(document).ready(function () {
 
-    var x = d3.scaleLinear()
-        .range([0, width])
-        .domain([0,10]);
-    var y = d3.scaleLinear()
-        .range([height, 0]);
+    'use strict';
 
-    var histogram = d3.histogram()
-        .value(function(d) { return d.OverallQual; })
-        .domain(x.domain());
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
 
-    d3.csv("data/train.csv").then(function(data){
+    trigger.click(function () {
+        hamburger_cross();
+    });
 
-        console.log(data);
-        // group the data for the bars
-        var bins = histogram(data);
+    function hamburger_cross() {
 
-        // Scale the range of the data in the y domain
-        y.domain([0, d3.max(bins, function(d) { return d.length; })]);
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
 
-        // append the bar rectangles to the svg element
-        svg.selectAll("rect")
-            .data(bins)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", 1)
-            .attr("transform", function(d) {
-                return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-            .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-            .attr("height", function(d) { return height - y(d.length); });
+    // ========================================================================= //
+    //  Typed Js
+    // ========================================================================= //
 
-        // add the x Axis
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+    var typed = $(".typed");
 
-        // add the y Axis
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
+    $(function () {
+        typed.typed({
+            strings: [
+                "Cleaning",
+                "Integration",
+                "Processing",
+                "Analytics",
+                "Science"
+            ],
+            typeSpeed: 100,
+            loop: true
+        });
     });
 
 
-}
+    var panorama = [];
+    var viewer = [];
 
-main();
+
+    function initialize() {
+        var streetviewService = new google.maps.StreetViewService;
+        streetviewService.getPanorama(
+            {location: {lat: -33.867386, lng: 151.195767}},
+            function (result, status) {
+                if (status === 'OK') {
+
+                    $('.pano-container').each(function (index) {
+
+                        panorama[index] = new PANOLENS.GoogleStreetviewPanorama(result.location.pano);
+                        viewer[index] = new PANOLENS.Viewer({container: $(this)[0]});
+                        viewer[index].add(panorama[index]);
+                        viewer[index].OrbitControls.noZoom = true;
+
+                        viewer[index].addUpdateCallback(function () {
+
+                            viewer[index].panorama.rotation.y -= 0.001;
+
+                        });
+
+
+                    });
+
+                }
+            });
+    }
+
+    initialize();
+
+    $('#blogCarousel').carousel({
+        interval: 3000
+    });
+
+
+});
