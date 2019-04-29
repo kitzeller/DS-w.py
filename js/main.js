84ccd0a3b1e5a6efc2bf7ca682240f87d82d9b17
kitzeller <kit.zeller@gmail.com>
2019-04-28 21:31:52 -0400

    Added linear regression

--- js/main.js
+++ js/main.js
@@ -98,6 +98,35 @@ $(document).ready(function () {
     $('#blogCarousel').carousel({
         interval: 3000
     });
-
-
+    
 });
+
+function testResults(form) {
+    var x1 = parseInt(form.grliv.value);
+    var x2 = parseInt(form.overall_qual.value);
+    var x3 = parseInt(form.year.value);
+    var x4 = parseInt(form.totalbsmt.value);
+    var x5 = x4 * 0.41592;
+    var x6 = parseInt(form.overall_cond.value);
+    var x7 = parseInt(form.mszoning.value);
+    var x8 = parseInt(form.garage.value);
+    var x9 = parseInt(form.fireplaces.value);
+    var x10 = parseInt(form.pconc.value);
+
+
+    var c1 = 0.13175142;
+    var c2 = 0.09435117;
+    var c3 = 0.07946046;
+    var c4 = 0.06444939;
+    var c5 = 0.03267284;
+    var c6 = 0.06919398;
+    var c7 = -0.03910227;
+    var c8 = 0.0584185;
+    var c9 = 0.03550599;
+    var c10 = 0.03217456;
+
+    var y = Math.exp(x1*c1 + x2*c2 + x3*c3 + x4*c4 + x5*c5 + x6*c6 + x7*c7 + x8*c8 + x9*c9 + x10*c10);
+
+    document.getElementById("result").value = "$" + y.toPrecision(4);
+
+}
